5a33e25c85c9aeb15aa49c441076904919911ea9
Takeshi Abe <tabe@fixedpoint.jp>
2010-01-21 11:17:24 +0900

    A happy new year

--- sitelib/smathml/content.scm
+++ sitelib/smathml/content.scm
@@ -1,5 +1,5 @@
 ;;
-;;   Copyright (c) 2009 Takeshi Abe. All rights reserved.
+;;   Copyright (c) 2009,2010 Takeshi Abe. All rights reserved.
 ;;
 ;;   Redistribution and use in source and binary forms, with or without
 ;;   modification, are permitted provided that the following conditions
@@ -84,7 +84,7 @@
          ,(smathml x prefix)
          ...))))
 
-  ;; completed implicitly:
+  ;; to be completed implicitly:
   ;; - apply
   ;; - sep
   (define-syntax smathml
